25eaab325fa3f4e68367f98ec23be221d454ecd9
Akihiro Oyamada <admin@yomotsu.net>
2025-05-30 12:18:02 +0900

    distDir

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/sub',
   output: 'export', // Use 'export' for static export
+  distDir: 'out/sub',
 };
 
 export default nextConfig;
